c4e6df38c104311090f7041a1776b25e42ceae0f
HiTTWorld <jtkim98111@gmail.com>
2024-05-25 18:35:54 +0900

    first commit

---
+++ python.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+
+# Set the title of the main page
+st.title("Movie Project")
+
+# Add items to the sidebar
+with st.sidebar:
+    st.header("Getting started")
+    st.write("Demo: Survey")
+    st.write("Demo: User management")
